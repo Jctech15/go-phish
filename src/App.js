bbd497323abb3efcb58c456b3a302c82d0c821d4
Aaron Na <asynchna@gmail.com>
2023-09-16 13:35:30 -0700

    got the layout to look more like MS one

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import "./App.css";
 import mslogo from "../src/ms.logo.jpg";
+import optionsKey from "../src/microsoft key sign in options.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <div className="container">
           <div className="login-box">
             <img src={mslogo} alt="" />
-            <div>Sign In</div>
+            <h2 style={{ margin: "auto" }}>Sign In</h2>
             <input
               className="loginfmt"
               type="email"
               placeholder="Email, phone, or Skype"
             />
-            <div>No Account? Create One</div>
-            <div>Can't access your account?</div>
-            <div>
-              <button type="button">Back</button>
-              <button type="submit">Next</button>
+            <p className="loginCTA">
+              No Account? <a href="www.google.com">Create One!</a>
+            </p>
+            <p className="loginCTA">
+              <a href="www.google.com">Can't access your account?</a>
+            </p>
+            <div className="buttonGroup">
+              <button
+                className="backButton"
+                type="button"
+                style={{ marginRight: "5px" }}
+              >
+                Back
+              </button>
+              <button className="nextButton" type="submit">
+                Next
+              </button>
             </div>
           </div>
+        </div>
+        <div className="optionsContainer">
+          <img
+            src={optionsKey}
+            style={{ height: "32px", paddingRight: "1rem" }}
+            alt=""
+          />
           <div className="other-login">Sign-in options</div>
         </div>
       </header>
