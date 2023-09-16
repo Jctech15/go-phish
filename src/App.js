cda4884a1ec5032aaa74d1a2937afce1ace95686
Jctech15 <jyctech15@gmail.com>
2023-09-16 11:53:16 -0700

    outline of project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import mslogo from "../src/ms.logo.jpg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="container">
+          <div className="login-box">
+            <img src={mslogo} alt="" />
+            <div>Sign In</div>
+            <input
+              className="loginfmt"
+              type="email"
+              placeholder="Email, phone, or Skype"
+            />
+            <div>No Account? Create One</div>
+            <div>Can't access your account?</div>
+            <div>
+              <button type="button">Back</button>
+              <button type="submit">Next</button>
+            </div>
+          </div>
+          <div className="other-login">Sign-in options</div>
+        </div>
       </header>
     </div>
   );
