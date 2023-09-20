1b82e1f5be7f317b82247ef00bcee89739b74408
Jctech15 <jyctech15@gmail.com>
2023-09-20 12:09:50 -0700

    finished the display changes with the gradient and options container separation

--- src/App.js
+++ src/App.js
@@ -1,49 +1,52 @@
 import "./App.css";
 import mslogo from "../src/ms.logo.jpg";
 import optionsKey from "../src/microsoft key sign in options.svg";
+import { useEffect } from "react";
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <div className="container">
-          <div className="login-box">
-            <img src={mslogo} alt="" />
-            <h2 style={{ margin: "auto" }}>Sign In</h2>
-            <input
-              className="loginfmt"
-              type="email"
-              placeholder="Email, phone, or Skype"
-            />
-            <p className="loginCTA">
-              No Account? <a href="www.google.com">Create One!</a>
-            </p>
-            <p className="loginCTA">
-              <a href="www.google.com">Can't access your account?</a>
-            </p>
-            <div className="buttonGroup">
-              <button
-                className="backButton"
-                type="button"
-                style={{ marginRight: "5px" }}
-              >
-                Back
-              </button>
-              <button className="nextButton" type="submit">
-                Next
-              </button>
-            </div>
+      <div className="container">
+        <div className="login-box">
+          <img src={mslogo} alt="" />
+          <h2
+            style={{
+              margin: "auto",
+              marginBottom: "10px",
+              marginTop: "10px",
+            }}
+          >
+            Sign In
+          </h2>
+          <input
+            className="loginfmt"
+            type="email"
+            placeholder="Email, phone, or Skype"
+          />
+          <p className="loginCTA">
+            No Account? <a href="www.google.com">Create One!</a>
+          </p>
+          <p className="loginCTA">
+            <a href="www.google.com">Can't access your account?</a>
+          </p>
+          <div className="buttonGroup">
+            <button
+              className="backButton"
+              type="button"
+              style={{ marginRight: "5px" }}
+            >
+              Back
+            </button>
+            <button className="nextButton" type="submit">
+              Next
+            </button>
           </div>
         </div>
-        <div className="optionsContainer">
-          <img
-            src={optionsKey}
-            style={{ height: "32px", paddingRight: "1rem" }}
-            alt=""
-          />
-          <div className="other-login">Sign-in options</div>
-        </div>
-      </header>
+      </div>
+      <div className="optionsContainer">
+        <img id="keylogo" src={optionsKey} alt="" />
+        <div className="other-login">Sign-in options</div>
+      </div>
     </div>
   );
 }
